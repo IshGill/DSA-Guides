e85160630e011ce6029d1840c9921c95c890ea9a
Ish Gill <Ish-gill@hotmail.com>
2021-03-13 10:28:53 +1300

    Add files via upload

---
+++ Shapes/Iso_triangle.py
@@ -0,0 +1,8 @@
+def draw_triangle(default=5):
+    print(default)
+
+if __name__ == '__main__':
+    draw_triangle() #Notice no parameter is passed
+    draw_triangle(2)
+    draw_triangle(10)
+    draw_triangle("two")
