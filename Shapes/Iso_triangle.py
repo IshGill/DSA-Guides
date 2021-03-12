34f35c20572766167973db34a196d06aa099c65d
Ish Gill <Ish-gill@hotmail.com>
2021-03-13 10:29:37 +1300

    Delete Iso_triangle.py

--- Shapes/Iso_triangle.py
+++
@@ -1,8 +0,0 @@
-def draw_triangle(default=5):
-    print(default)
-
-if __name__ == '__main__':
-    draw_triangle() #Notice no parameter is passed
-    draw_triangle(2)
-    draw_triangle(10)
-    draw_triangle("two")
